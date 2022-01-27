adcc12ccd2536307709c8dd198718be3decb50f3
William Onnyx <wonnyxiforus@gmail.com>
2022-01-27 17:13:45 +0100

    adding custom router

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
@@ -18,7 +18,10 @@ const DUMYY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
+
+  // console.log(match);
 
   const quote = DUMYY_QUOTES.find((quote) => quote.id === params.quoteId);
 
@@ -29,7 +32,12 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}/comments`} exact>
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>Load Comments</Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`} exact>
         <Comments />
       </Route>
     </Fragment>
